a0bf0cc54d447d2f5cdd2c8b244fa1316e2ce696
Noel <112729963+NoelH98@users.noreply.github.com>
2022-09-17 21:25:32 +0300

    Delete react-calculator directory

--- react-calculator/DigitButton.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from './App'
-
-export default function DigitButton({ dispatch, digit }){
-	return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, results: {digit} })}>{digit}</button>
-}
--- react-calculator/OperaButton.js
+++
@@ -1,5 +0,0 @@
-import { ACTIONS } from './App'
-
-export default function OperaButton({ dispatch, operation }){
-	return <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, results: {operation} })}>{operation}</button>
-}
